2b7f676dd5a93f5b739b59c897a7d4c6fedbbb56
Denilson Behenck <denilsontrespa10@gmail.com>
2022-09-17 21:34:20 -0300

    server

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,6 @@ app.get("/ads", (request, response) => {
     { id: 1, nome: 'Anúncio 1'},
     { id: 2, nome: 'Anúncio 2'},
     { id: 3, nome: 'Anúncio 3'},
-    { id: 4, nome: 'Anúncio 4'},
   ])
 })
 
